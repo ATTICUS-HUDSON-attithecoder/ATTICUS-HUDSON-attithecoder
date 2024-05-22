821eedfbecbe2952af6d55cc8cff67ec4737e8a3
ATTICUS-HUDSON-attithecoder <atticuswb@outlook.com>
2024-05-22 13:01:27 -0700

    Update Yo_mama_roast.py

--- Yo_mama_roast.py
+++ Yo_mama_roast.py
@@ -1,6 +1,12 @@
 print(‘To receive a yo mama roast, press the keys 1, 2, 3, 4, or, 5.’)
 if (keyboard_input.(‘1’))
   print(‘’’Yo mama is so fat that…
+
   When she went to the fat contest they said…
-  “NO PROFFESSIONALS ALLOWED”!’’’)
-if (
+
+
+  “NO PROFFESSIONALS ALLOWED!"’’’)
+if (keyboard_input.(2))
+  print('''Yo mama's so fat that...
+
+  she starts the alphabet with "o": o,d,c,d(sounds like obesity)''')
